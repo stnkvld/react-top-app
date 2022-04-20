ea94a37d2ab5d4b83d2d269d59b1f1c6ba17deec
Владислав Сотников <stnkvld@gmail.com>
2022-04-20 14:31:57 +0300

    Правки по Я.Метрике

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,17 +1,18 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import React from 'react';
+import Router from 'next/router';
 import '../styles/globals.css';
 import ym from 'react-yandex-metrika';
 import { YMInitializer } from 'react-yandex-metrika';
 
-function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
-  router.events.on('routeChangeComplete', (url: string) => {
-    if (typeof window !== undefined) {
-      ym('hit', url);
-    }
-  });
+Router.events.on('routeChangeComplete', (url: string) => {
+  if (typeof window !== undefined) {
+    ym('hit', url);
+  }
+});
 
+function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
   return <>
     <Head>
       <title>MyTop</title>
